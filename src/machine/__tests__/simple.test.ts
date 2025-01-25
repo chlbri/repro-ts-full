46335c100d563db01dfe6f874b9e181887038959
chlbri <bri_lvi@icloud.com>
2025-01-25 17:38:58 +0000

    add

--- src/machine/__tests__/simple.test.ts
+++ src/machine/__tests__/simple.test.ts
@@ -1,25 +1,13 @@
-import { machine1 } from "./fixtures";
+import { machine1 } from './fixtures';
 
 describe('simple', () => {
   test('#0 => Initialize machine', () => {});
-
-  test('#1 => Before all, simple is empty', () => {
-    expect(machine1.simple).toBeUndefined();
-  });
-
-  test('#2 => ProbideInitials', () => {
-    machine1.addInitials({
-      '/': 'state1',
-      '/state1': 'state11',
-      '/state1/state11': 'state111',
-    } as any);
-  });
 
   test.todo('Console.log', () => {
     console.log(JSON.stringify(machine1.simple, null, 2));
   });
 
-  test('#3 => simple is defined, check it !', () => {
+  test('#1 => simple is defined, check it !', () => {
     const expected = {
       type: 'compound',
       entry: [],
